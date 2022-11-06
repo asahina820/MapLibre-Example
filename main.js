dfc8de4e2875494b06f45800555f25e07156abe1
hinamei <hina.gntm@gmail.com>
2022-11-06 13:27:06 +0900

    flytoを実装

--- main.js
+++ main.js
@@ -1,67 +1,37 @@
+var haneda_location = [139.7663947, 35.5437833];
+var shinchitose_location = [141.681229, 42.7875897];
+
 var map = new maplibregl.Map({
     container: 'map',
     style: 'https://tile.openstreetmap.jp/styles/osm-bright-ja/style.json', // 地図のスタイル
-    center: [140.067575487, 35.791039821], // 中心座標
-    zoom: 18, // ズームレベル
+    center: haneda_location, // 中心座標
+    zoom: 11, // ズームレベル
     pitch: 0, // 傾き
 });
 
-var hoveredStateId = null;
+var haneda_marker = new maplibregl.Marker()
+    .setLngLat(haneda_location)
+    .addTo(map);
 
-map.on('load', function () {
-    map.addSource('funabashi_fudepolygon', {
-        type: 'geojson',
-        data: './data/funabashi_fudepolygon.geojson',
-    });
-    map.addLayer({
-        id: 'funabashi_fudepolygon_fill',
-        type: 'fill',
-        source: 'funabashi_fudepolygon',
-        layout: {},
-        paint: {
-            'fill-color': '#84C98B',
-            'fill-opacity': [
-                'case',
-                ['boolean', ['feature-state', 'hover'], false],
-                1,
-                0.5,
-            ],
+var shinchitose_marker = new maplibregl.Marker()
+    .setLngLat(shinchitose_location)
+    .addTo(map);
+
+var isAtStart = true;
+
+document.getElementById('fly').addEventListener('click', function () {
+    var target = isAtStart ? shinchitose_location : haneda_location;
+    isAtStart = !isAtStart;
+
+    map.flyTo({
+        center: target,
+        zoom: 9,
+        bearing: 0,
+        speed: 0.4,
+        curve: 1,
+        easing: function (t) {
+            return t;
         },
-    });
-    map.addLayer({
-        id: 'funabashi_fudepolygon_border',
-        type: 'line',
-        source: 'funabashi_fudepolygon',
-        layout: {},
-        paint: {
-            'line-color': '#2B6442',
-            'line-width': 1,
-        },
-    });
-
-    map.on('mousemove', 'funabashi_fudepolygon_fill', function (e) {
-        if (e.features.length > 0) {
-            if (hoveredStateId) {
-                map.setFeatureState(
-                    { source: 'funabashi_fudepolygon', id: hoveredStateId },
-                    { hover: false }
-                );
-            }
-            hoveredStateId = e.features[0].id;
-            map.setFeatureState(
-                { source: 'funabashi_fudepolygon', id: hoveredStateId },
-                { hover: true }
-            );
-        }
-    });
-
-    map.on('mouseleave', 'funabashi_fudepolygon_fill', function () {
-        if (hoveredStateId) {
-            map.setFeatureState(
-                { source: 'funabashi_fudepolygon', id: hoveredStateId },
-                { hover: false }
-            );
-        }
-        hoveredStateId = null;
+        essential: true,
     });
 });
