8163c609c956b92d4f93ba57b130abfa5886e1f9
hinamei <hina.gntm@gmail.com>
2022-11-23 23:02:09 +0900

    アイコンにアニメーションを追加

--- main.js
+++ main.js
@@ -1,38 +1,58 @@
-const tokyo_location = [139.74019, 35.6642];
-const start_location = [136.29, 38.84];
-
 var map = new maplibregl.Map({
     container: 'map',
-    style: 'https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json', // 地図のスタイル
-    center: start_location, // 中心座標
-    zoom: 5, // ズームレベル
+    style: 'https://tile.openstreetmap.jp/styles/osm-bright-ja/style.json', // 地図のスタイル
+    center: [139.74019, 35.6642], // 中心座標
+    zoom: 14, // ズームレベル
     pitch: 0, // 傾き
 });
 
+var size = 200;
+
+// アイコンのアニメージョンの設定
+var pulsingDot = {
+    width: size,
+    height: size,
+    data: new Uint8Array(size * size * 4),
+
+    onAdd: function () {
+        var canvas = document.createElement('canvas');
+        canvas.width = this.width;
+        canvas.height = this.height;
+        this.context = canvas.getContext('2d');
+    },
+
+    render: function () {
+        var duration = 1000;
+        var t = (performance.now() % duration) / duration;
+
+        var radius = (size / 2) * 0.3;
+        var outerRadius = (size / 2) * 0.7 * t + radius;
+        var context = this.context;
+
+        context.clearRect(0, 0, this.width, this.height);
+        context.beginPath();
+        context.arc(this.width / 2, this.height / 2, outerRadius, 0, Math.PI * 2);
+        context.fillStyle = 'rgba(255, 200, 200,' + (1 - t) + ')';
+        context.fill();
+
+        context.beginPath();
+        context.arc(this.width / 2, this.height / 2, radius, 0, Math.PI * 2);
+        context.fillStyle = 'rgba(255, 100, 100, 1)';
+        context.strokeStyle = 'white';
+        context.lineWidth = 2 + 4 * (1 - t);
+        context.fill();
+        context.stroke();
+
+        this.data = context.getImageData(0, 0, this.width, this.height).data;
+
+        map.triggerRepaint();
+
+        return true;
+    },
+};
+
 map.on('load', function () {
-    map.addSource('railroad', {
-        type: 'geojson',
-        data: './data/railroad.geojson',
-    });
-    map.addLayer({
-        id: 'railroad',
-        type: 'line',
-        source: 'railroad',
-        minzoom: 0,
-        layout: {
-            'line-join': 'round',
-            'line-cap': 'round',
-        },
-        paint: {
-            'line-color': '#ee7800',
-            'line-opacity': 0.7,
-            'line-width': 1.5,
-        },
-    });
-    map.loadImage('./data/station.png', function (error, image) {
-        if (error) throw error;
-        map.addImage('station_icon', image);
-    });
+    map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
     map.addSource('red_station', {
         type: 'geojson',
         data: './data/red_station.geojson',
@@ -42,41 +62,13 @@ map.on('load', function () {
         id: 'red_station',
         type: 'symbol',
         source: 'red_station',
-        minzoom: 6,
         layout: {
-            'icon-image': 'station_icon',
-            'icon-size': 0.15,
+            'icon-image': 'pulsing-dot',
+            'icon-size': 0.7,
             'text-field': ['get', 'station'],
             'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
             'text-offset': [0, 1.2],
             'text-anchor': 'top',
         },
-        paint: {
-            'text-color': '#ffffff',
-        },
-    });
-    map.addLayer({
-        id: 'red_station_circle',
-        source: 'red_station',
-        maxzoom: 6,
-        type: 'circle',
-        paint: {
-            'circle-radius': 4,
-            'circle-color': 'red',
-        },
-    });
-});
-
-document.getElementById('fly').addEventListener('click', function () {
-    map.flyTo({
-        center: tokyo_location,
-        zoom: 14,
-        bearing: 0,
-        speed: 1.1,
-        curve: 1.42,
-        easing: function (t) {
-            return t;
-        },
-        essential: true,
     });
 });
