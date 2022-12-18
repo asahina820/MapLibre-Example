2665c0d70d98c30ba986910abd52492fada3eb91
hinamei <hina.gntm@gmail.com>
2022-12-18 17:01:10 +0900

    ベクトルタイルのスタイル追加

--- main.js
+++ main.js
@@ -1,47 +1,52 @@
-const style = {
+style = {
     version: 8,
     sources: {
-        rtile: {
+        // 地理院タイル（標準地図）
+        'gsi-tile': {
             type: 'raster',
             tiles: ['https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png'],
             tileSize: 256,
-            attribution: "地図の出典：<a href='https://www.gsi.go.jp/' target='_blank'>地理院タイル</a>",
+            attribution: "地図の出典：<a href='https://www.gsi.go.jp/' target='_blank'>地理院タイル（標準）</a>",
         },
-        plateau: {
+        //地理院ベクトルタイル
+        'gsi-vector': {
             type: 'vector',
-            tiles: ['https://indigo-lab.github.io/plateau-tokyo23ku-building-mvt-2020/{z}/{x}/{y}.pbf'],
-            minzoom: 10,
-            maxzoom: 16,
-            attribution:
-                "データの出典：<a href='https://github.com/indigo-lab/plateau-tokyo23ku-building-mvt-2020'>plateau-tokyo23ku-building-mvt-2020 by indigo-lab</a> (<a href='https://www.mlit.go.jp/plateau/'>国土交通省 Project PLATEAU</a> のデータを加工して作成)",
+            tiles: ['https://cyberjapandata.gsi.go.jp/xyz/experimental_bvmap/{z}/{x}/{y}.pbf'],
+            minZoom: 0,
+            maxZoom: 14,
+            maxzoom: 18,
+            attribution: "建物データの出典：<a href='https://www.gsi.go.jp/' target='_blank'>国土地理院ベクトルタイル提供実験</a>",
         },
     },
     layers: [
+        // 背景地図として国土地理院の地理院タイル（標準地図）を追加
         {
-            id: 'rtile',
+            id: 'gsi-tile',
             type: 'raster',
-            source: 'rtile',
-            minzoom: 5,
-            maxzoom: 20,
+            source: 'gsi-tile',
+            minzoom: 0,
+            maxzoom: 24,
         },
+
+        // 地理院ベクトルタイルの建物を追加
         {
-            id: 'bldg',
+            id: 'gsi-building',
             type: 'fill-extrusion',
-            source: 'plateau',
-            'source-layer': 'bldg',
-            minzoom: 10,
-            maxzoom: 20,
+            source: 'gsi-vector',
+            'source-layer': 'building',
+            layout: { visibility: 'visible' },
             paint: {
-                'fill-extrusion-color': '#797979',
-                'fill-extrusion-height': ['get', 'measuredHeight'],
+                'fill-extrusion-color': '#C0C0C0',
+                'fill-extrusion-height': 50,
             },
         },
     ],
 };
+
 var map = new maplibregl.Map({
     container: 'map',
     style: style, // 地図のスタイル
-    center: [139.745461, 35.65856], // 中心座標
-    zoom: 14, // ズームレベル
-    pitch: 45, // 傾き
+    center: [139.7673068, 35.6809591], // 中心座標
+    zoom: 15, // ズームレベル
+    pitch: 30, // 傾き
 });
